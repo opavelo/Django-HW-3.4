a8a236b4396a519262ffb3e9c65c20849bc84318
opavelo <opavelo@gmail.com>
2021-09-12 22:04:15 +0300

    second edit

--- django_testing/tests/students/test_courses_api.py
+++ django_testing/tests/students/test_courses_api.py
@@ -1,6 +1,7 @@
 from rest_framework.test import APIClient
 from django.urls import reverse
 import pytest
+import random
 from students.models import Course
 from model_bakery import baker
 from students.serializers import CourseSerializer
@@ -10,15 +11,15 @@ import json
 # Проверка получения 1го курса (retrieve-логика)
 @pytest.mark.django_db
 def test_1st_courses(client, course_factory):
-    objects = course_factory(_quantity=5)
-    print('obj:', objects)
-    # Use a DRF ModelSerializer to give us JSON
-    for object in objects:
-        payload = CourseSerializer(object).data
-        print('payload:', payload)
-        url = reverse("course-list")
-        response = client.post(url, payload, format='json')
-        assert response.status_code == 201
+    object = course_factory(_quantity=1)
+    print('obj:', object)
+    payload = CourseSerializer(object[0]).data
+    print('payload:', payload)
+    url = reverse('course-detail', args=[str(payload['id'])])
+    print('url', url)
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.data['name'] == payload['name']
 
 
 # Проверка получения списка курсов (list-логика)
@@ -34,32 +35,48 @@ def test_courses(client, course_factory):
 # Проверка фильтрации списка курсов по id
 @pytest.mark.django_db
 def test_filter_id_courses(client, course_factory):
-    objs = course_factory(_quantity=10)
-    objs = Course.objects.filter(id=1)
-    print('obj:', objs)
-    for object in objs:
-        payload = CourseSerializer(object).data
-        print('payload:', payload)
-        url = reverse("course-list")
-        response = client.post(url, payload, format='json')
-        print('response', response.data)
-        assert response.status_code == 201
+    object = course_factory(_quantity=10)
+    id_list = []
+    for item in object:
+        payload = CourseSerializer(item).data
+        print('payload:', payload['id'])
+        id_list.append(payload['id'])
+
+    id_random = random.choice(id_list)
+    print(id_random)
+
+    payload = {
+        'id': id_random,
+    }
+
+    url = reverse("course-list")
+    response_filter = client.get(url, payload, format='json')
+    print('response_filter: ', response_filter.data)
+    assert response_filter.status_code == 200
 
 
 # Проверка фильтрации списка курсов по name
 @pytest.mark.django_db
 def test_filter_name_courses(client, course_factory):
-    objs = course_factory(_quantity=10)
-    objs = Course.objects.filter(name='Math')
-    print('obj:', objs)
-    for object in objs:
-        payload = CourseSerializer(object).data
-        print('payload:', payload)
-        url = reverse("course-list")
-        response = client.post(url, payload, format='json')
-        print('response', response.data)
-        assert response.status_code == 201
-    assert len(objs) == 0
+    object = course_factory(_quantity=10)
+    name_list = []
+    for item in object:
+        payload = CourseSerializer(item).data
+        print('payload:', payload['name'])
+        name_list.append(payload['name'])
+    name_random = random.choice(name_list)
+    print(name_random)
+
+    payload = {
+        'name': name_random,
+    }
+
+    url = reverse("course-list")
+    response_filter = client.get(url, payload, format='json')
+    print('response_filter: ', response_filter.data)
+    assert response_filter.status_code == 200
+
+
 
 # Тест успешного создания курса
 @pytest.mark.django_db
@@ -78,48 +95,32 @@ def test_create_course(client):
 # Тест успешного обновления курса
 @pytest.mark.django_db
 def test_patch_course(client, course_factory):
-
     object = course_factory()
     print('obj:', object)
     payload = CourseSerializer(object).data
     print('payload:', payload)
-    url = reverse("course-list")
-    response = client.post(url, payload, format='json')
-    response = response.json()
-    name = response['name']
-    print('name:', name)
-    payload = {
-        'id': 1,
+    url = reverse('course-detail', args=[str(payload['id'])])
+    payload2 = {
+        'id': payload['id'],
         'name': 'Math',
     }
-    # url = reverse('course-detail')
-    url = reverse('course-list')
-    url = url + str(payload['id'])
-    response = client.patch(url, payload, format='json')
-    print('response', response)
-    assert response.status_code == 301
-
+    response = client.patch(url, payload2, format='json')
+    print('response_name', response.data['name'])
+    assert response.data['name'] == payload2['name']
+    assert response.status_code == 200
 
 # Тест успешного удаления курса
 @pytest.mark.django_db
 def test_delete_course(client, course_factory):
-
     object = course_factory()
     print('obj:', object)
     payload = CourseSerializer(object).data
     print('payload:', payload)
-    url = reverse("course-list")
-    response = client.post(url, payload, format='json')
-    response = response.json()
-    name = response['name']
-    print('name:', name)
-    payload = {
-        'id': 1,
-        'name': 'Math',
-    }
-    # url = reverse('course-detail')
-    url = reverse('course-list')
-    url = url + str(payload['id'])
+    url = reverse('course-detail', args=[str(payload['id'])])
+    # Проверяем наличие
+    response = client.get(url)
+    assert response.data['id'] == payload['id']
+    # Проверяем удаление
     response = client.delete(url, payload, format='json')
     print('response', response)
-    assert response.status_code == 301
+    assert response.status_code == 204
